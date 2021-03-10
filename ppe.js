14461218af43b0604db9dbaebed23e4cc9ad8543
40823137 <55519000+40823137@users.noreply.github.com>
2021-03-10 14:39:00 +0800

    Add files via upload

---
+++ ppe.js
@@ -0,0 +1,36 @@
+
+function change1() {
+ 
+    document.getElementById("image").src="01pp.png";
+    
+  };
+function change2() {
+ 
+    document.getElementById("image").src="02pp.png";
+
+};
+function change3() {
+ 
+    document.getElementById("image").src="03pp.png";
+
+};
+function change4() {
+ 
+    document.getElementById("image").src="04pp.png";
+
+};
+function change5() {
+ 
+    document.getElementById("image").src="05pp.png";
+
+};
+function change6() {
+ 
+    document.getElementById("image").src="06pp.png";
+
+};
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
+  });
